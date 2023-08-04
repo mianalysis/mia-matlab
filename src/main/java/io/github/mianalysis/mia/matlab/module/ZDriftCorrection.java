75a8264a727d3a3b85f8988ce6dff42e64bba1ad
Stephen Cross <stephencross86@gmail.com>
2023-08-04 15:50:17 +0100

    Added code for ZDriftCorrection module

--- src/main/java/io/github/mianalysis/mia/matlab/module/ZDriftCorrection.java
+++ src/main/java/io/github/mianalysis/mia/matlab/module/ZDriftCorrection.java
@@ -3,13 +3,27 @@ package io.github.mianalysis.mia.matlab.module;
 import org.scijava.Priority;
 import org.scijava.plugin.Plugin;
 
+import com.mathworks.toolbox.javabuilder.MWException;
+import com.mathworks.toolbox.javabuilder.MWNumericArray;
+
+import MIA_MATLAB_Core.StackMatcher;
+import ij.ImagePlus;
+import io.github.mianalysis.mia.MIA;
 import io.github.mianalysis.mia.module.AvailableModules;
 import io.github.mianalysis.mia.module.Categories;
 import io.github.mianalysis.mia.module.Category;
 import io.github.mianalysis.mia.module.Module;
 import io.github.mianalysis.mia.module.Modules;
+import io.github.mianalysis.mia.module.images.transform.ExtractSubstack;
+import io.github.mianalysis.mia.module.images.transform.FocusStackGlobal;
 import io.github.mianalysis.mia.object.Workspace;
+import io.github.mianalysis.mia.object.image.Image;
+import io.github.mianalysis.mia.object.image.ImageFactory;
+import io.github.mianalysis.mia.object.parameters.InputImageP;
+import io.github.mianalysis.mia.object.parameters.OutputImageP;
 import io.github.mianalysis.mia.object.parameters.Parameters;
+import io.github.mianalysis.mia.object.parameters.SeparatorP;
+import io.github.mianalysis.mia.object.parameters.text.IntegerP;
 import io.github.mianalysis.mia.object.refs.collections.ImageMeasurementRefs;
 import io.github.mianalysis.mia.object.refs.collections.MetadataRefs;
 import io.github.mianalysis.mia.object.refs.collections.ObjMeasurementRefs;
@@ -20,7 +34,15 @@ import net.imagej.ImageJ;
 
 @Plugin(type = Module.class, priority = Priority.LOW, visible = true)
 public class ZDriftCorrection extends CoreMATLABModule {
-    public static void main(String[] args) {        
+    private static final String INPUT_SEPARATOR = "Image input/output";
+    public static final String INPUT_IMAGE = "Input image";
+    public static final String OUTPUT_IMAGE = "Output image";
+
+    private static final String DRIFT_CORRECTION_SEPARATOR = "Drift correction controls";
+    public static final String REFERENCE_IMAGE = "Reference image";
+    public static final String CALCULATION_CHANNEL = "Calculation channel";
+
+    public static void main(String[] args) {
         // Creating a new instance of ImageJ
         new ij.ImageJ();
 
@@ -48,14 +70,66 @@ public class ZDriftCorrection extends CoreMATLABModule {
 
     @Override
     protected Status process(Workspace workspace) {
-        Module.writeProgressStatus(0, 0, description, name);
+        // Getting input image
+        String inputImageName = parameters.getValue(INPUT_IMAGE, workspace);
+        String outputImageName = parameters.getValue(OUTPUT_IMAGE, workspace);
+        String refImageName = parameters.getValue(REFERENCE_IMAGE, workspace);
+        int calculationChannel = parameters.getValue(CALCULATION_CHANNEL, workspace);
+
+        // Getting input images
+        Image inputImage = workspace.getImage(inputImageName);
+        ImagePlus inputIpl = inputImage.getImagePlus();
+        Image refImage = workspace.getImage(refImageName);
+        MWNumericArray refArray = imageStackToMW(refImage.getImagePlus().getStack());
+
+        StackMatcher stackMatcher;
+        try {
+            stackMatcher = new StackMatcher();
+        } catch (MWException e) {
+            MIA.log.writeError(e);
+            return Status.FAIL;
+        }
+
+        int[] bestSlices = new int[inputIpl.getNFrames()];
+        for (int t = 1; t <= inputIpl.getNFrames(); t++) {
+            Image substack = ExtractSubstack.extractSubstack(inputImage, "Substack", String.valueOf(calculationChannel),
+                    "1-end", String.valueOf(t));
+            MWNumericArray substackArray = imageStackToMW(substack.getImagePlus().getStack());
+
+            try {
+                Object[] output = stackMatcher.matchImageInStack(1, substackArray, refArray);
+                bestSlices[t - 1] = (int) output[0];
+                MIA.log.writeDebug(bestSlices[t - 1]);
+            } catch (MWException e) {
+                MIA.log.writeError(e);
+                return Status.FAIL;
+            }
+
+            substackArray.dispose();
+
+        }
+
+        Image outputImage = FocusStackGlobal.extract(inputImage,0,0,bestSlices,outputImageName);
+        
+        workspace.addImage(outputImage);
+
+        if (showOutput)
+            outputImage.show();
+
         return Status.PASS;
 
     }
 
     @Override
     protected void initialiseParameters() {
-        
+        parameters.add(new SeparatorP(INPUT_SEPARATOR, this));
+        parameters.add(new InputImageP(INPUT_IMAGE, this));
+        parameters.add(new OutputImageP(OUTPUT_IMAGE, this));
+
+        parameters.add(new SeparatorP(DRIFT_CORRECTION_SEPARATOR, this));
+        parameters.add(new InputImageP(REFERENCE_IMAGE, this));
+        parameters.add(new IntegerP(CALCULATION_CHANNEL, this, 1));
+
     }
 
     @Override
